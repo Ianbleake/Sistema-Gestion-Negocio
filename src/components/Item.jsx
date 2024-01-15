24862021b360e6f795e9ee005ad6bd3eaccb7c77
Ivan Rangel <arviluki@gmail.com>
2024-01-15 00:59:49 -0600

    Product Cards

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { toCapital } from '../helpers/toCapital'
 
-const Item = ( {producto} ) => {
+const Item = ({ producto }) => {
   return (
-    <div className="producto">
-        <img src={producto.imagen} alt={producto.titulo} />
-        <div>
-            <h4>{producto.titulo}</h4>
-            <p>Precio: ${producto.precio}</p>
-            <p>Categoría: {toCapital(producto.categoria)}</p>
-            <Link className="ver-mas" to={`/item/${producto.id}`}>Ver más</Link>
+    <div className="card1">
+      <img className='card1-img' src={producto.imagen} alt={producto.titulo} />
+      <div>
+        <h4 className='card1-title mb-2'>{producto.titulo}</h4>
+        <p className='card1-subtitle mb-4'>Categoría: {toCapital(producto.categoria)}</p>
+        <hr class="card1-divider mb-4"></hr>
+        <div className='card1-footer'>
+          <div className='card1-price'><span>$</span>{producto.precio}</div>
+          <button className="card1-btn"><Link to={`/item/${producto.id}`}>Ver más</Link></button>
         </div>
+      </div>
     </div>
   )
 }
