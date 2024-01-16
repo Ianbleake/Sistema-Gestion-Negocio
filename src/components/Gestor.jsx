752d2fc2fb875be8baa07c9ccb1c444d6a3e472f
Ivan Rangel <arviluki@gmail.com>
2024-01-16 14:08:41 -0600

    v1.2

--- src/components/Gestor.jsx
+++ src/components/Gestor.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 import { db } from "../firebase/config";
 
 const ProductList = () => {
@@ -21,50 +21,61 @@ const ProductList = () => {
         fetchProducts();
     }, []);
 
+    const eliminarProducto = async (id) => {
+        try {
+            const productoRef = doc(db, "Productos", id);
+            await deleteDoc(productoRef);
+            console.log('Producto eliminado correctamente');
+
+            // Recarga la página después de eliminar el producto
+            window.location.reload();
+        } catch (error) {
+            console.error('Error al eliminar el producto', error);
+        }
+    };
+
     return (
         <div className="bg-sky-100">
 
-            <div class="divTable greyGridTable">
-                <div class="divTableHeading">
-                    <div class="divTableRow">
-                        <div class="divTableHead">Producto</div>
-                        <div class="divTableHead">Categoria</div>
-                        <div class="divTableHead">Precio</div>
-                        <div class="divTableHead">-</div>
+            <div className="divTable greyGridTable">
+                <div className="divTableHeading">
+                    <div className="divTableRow">
+                        <div className="divTableHead">Producto</div>
+                        <div className="divTableHead">Categoria</div>
+                        <div className="divTableHead">Precio</div>
+                        <div className="divTableHead">-</div>
                     </div>
                 </div>
-                <div class="divTableBody">
+                <div className="divTableBody">
 
                     {products.map((product) => (
-                        <div class="divTableRow">
-                            <div className="divTableCell" key={product.id}>{product.titulo}</div>
-                            <div className="divTableCell" key={product.id}>{product.categoria}</div>
-                            <div className="divTableCell" key={product.id}>{product.precio}</div>
-                            <div class="divTableCell">
+                        <div className="divTableRow" key={product.id}>
+                            <div className="divTableCell">{product.titulo}</div>
+                            <div className="divTableCell">{product.categoria}</div>
+                            <div className="divTableCell">{product.precio}</div>
+                            <div className="divTableCell">
                                 <button
-                                    class="inline-flex items-center px-4 py-2 bg-red-600 transition ease-in-out delay-75 hover:bg-red-700 text-white text-sm font-medium rounded-md "
+                                    className="inline-flex items-center px-4 py-2 bg-red-600 transition ease-in-out delay-75 hover:bg-red-700 text-white text-sm font-medium rounded-md "
+                                    onClick={() => eliminarProducto(product.id)}
                                 >
                                     <svg
                                         stroke="currentColor"
                                         viewBox="0 0 24 24"
                                         fill="none"
-                                        class="h-5 w-5 mr-2"
+                                        className="h-5 w-5 mr-2"
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path
                                             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                                            stroke-width="2"
-                                            stroke-linejoin="round"
-                                            stroke-linecap="round"
+                                            strokeWidth="2"
+                                            strokeLinejoin="round"
+                                            strokeLinecap="round"
                                         ></path>
                                     </svg>
-
                                     Delete
                                 </button>
-
                             </div>
                         </div>
-
                     ))}
 
                 </div>
--- src/components/Pedidos.jsx
+++ src/components/Pedidos.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 import { db } from "../firebase/config";
 
 export const Pedidos = () => {
@@ -21,69 +21,73 @@ export const Pedidos = () => {
         fetchProducts();
     }, []);
 
+    const eliminarPedido = async (id) => {
+        try {
+            const pedidoRef = doc(db, "Pedidos", id);
+            await deleteDoc(pedidoRef);
+            console.log('Pedido eliminado correctamente');
+
+            // Recarga la página después de eliminar el pedido
+            window.location.reload();
+        } catch (error) {
+            console.error('Error al eliminar el pedido', error);
+        }
+    };
+
     return (
         <div className="bg-sky-100">
-            <div class="divTable greyGridTable">
-                <div class="divTableHeading">
-                    <div class="divTableRow">
-                        <div class="divTableHead">Cliente</div>
-                        <div class="divTableHead">Mesa</div>
-                        <div class="divTableHead">Pedido</div>
-                        <div class="divTableHead">Total</div>
-                        <div class="divTableHead">-</div>
+            <div className="divTable greyGridTable">
+                <div className="divTableHeading">
+                    <div className="divTableRow">
+                        <div className="divTableHead">Cliente</div>
+                        <div className="divTableHead">Mesa</div>
+                        <div className="divTableHead">Pedido</div>
+                        <div className="divTableHead">Total</div>
+                        <div className="divTableHead">-</div>
                     </div>
                 </div>
-                <div class="divTableBody">
-
+                <div className="divTableBody">
                     {products.map((product) => (
-                        <div class="divTableRow">
-                            <div className="divTableCell" key={product.id}>{product.cliente.nombre}</div>
-                            <div className="divTableCell" key={product.id}>{product.cliente.mesa}</div>
-                            <div className="divTableCell" key={product.id}>
-                                {
-                                product.productos.map((producto)=>(
-                                    <ul>
+                        <div className="divTableRow" key={product.id}>
+                            <div className="divTableCell">{product.cliente.nombre}</div>
+                            <div className="divTableCell">{product.cliente.mesa}</div>
+                            <div className="divTableCell">
+                                {product.productos.map((producto) => (
+                                    <ul key={producto.id}>
                                         <li>
                                             <span className="mr-4">{producto.cantidad}</span>
                                             {producto.titulo}
                                         </li>
                                     </ul>
-                                    
                                 ))}
                             </div>
-                            
-                            <div className="divTableCell" key={product.id}>{product.total}</div>
-                            <div class="divTableCell">
+                            <div className="divTableCell">{product.total}</div>
+                            <div className="divTableCell">
                                 <button
-                                    class="inline-flex items-center px-4 py-2 bg-red-600 transition ease-in-out delay-75 hover:bg-red-700 text-white text-sm font-medium rounded-md "
+                                    className="inline-flex items-center px-4 py-2 bg-red-600 transition ease-in-out delay-75 hover:bg-red-700 text-white text-sm font-medium rounded-md"
+                                    onClick={() => eliminarPedido(product.id)}
                                 >
                                     <svg
                                         stroke="currentColor"
                                         viewBox="0 0 24 24"
                                         fill="none"
-                                        class="h-5 w-5 mr-2"
+                                        className="h-5 w-5 mr-2"
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path
                                             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                                            stroke-width="2"
-                                            stroke-linejoin="round"
-                                            stroke-linecap="round"
+                                            strokeWidth="2"
+                                            strokeLinejoin="round"
+                                            strokeLinecap="round"
                                         ></path>
                                     </svg>
-
                                     Delete
                                 </button>
-
                             </div>
                         </div>
-
                     ))}
-
                 </div>
-                
             </div>
-            
         </div>
     );
-}
+};
